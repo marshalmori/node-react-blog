e6a225ab7be8414b99dd6724e41095846587b568
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-01-04 21:22:52 -0300

    Create Sidebar component

--- src/sidebar/Sidebar.jsx
+++ src/sidebar/Sidebar.jsx
@@ -3,7 +3,44 @@ import './sidebar.css'
 export default function Sidebar() {
   return (
     <div className='sidebar'>
-        
+        <div className="sidebarItem">
+          <span className="sidebarTitle">ABOUT ME</span>
+          <img
+          src="https://images.pexels.com/photos/944773/pexels-photo-944773.jpeg"
+          alt="About me"
+          width="300"
+          height="250"
+          />
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+            Numquam debitis eligendi
+          </p>
+        </div>
+
+        <div className="sidebarItem">
+          <span className="sidebarTitle">CATEGORIES</span>
+          <ul className="sidebarList">
+            <li className="sidebarListItem">Life</li>
+            <li className="sidebarListItem">Music</li>
+            <li className="sidebarListItem">Style</li>
+            <li className="sidebarListItem">Sport</li>
+            <li className="sidebarListItem">Tech</li>
+            <li className="sidebarListItem">Cinema</li>
+          </ul>
+        </div>
+
+        <div className="sidebarItem">
+          <span className="sidebarTitle">FOLLOW US</span>
+          <div className="sidebarSocial">
+            <i className="sidebarIcon fa-brands fa-square-facebook"></i>
+            <i className="sidebarIcon fa-brands fa-square-twitter"></i>
+            <i className="sidebarIcon fa-brands fa-square-pinterest"></i>
+            <i className="sidebarIcon fa-brands fa-square-instagram"></i>
+          </div>
+          
+        </div>
+
+
     </div>
   )
 }
